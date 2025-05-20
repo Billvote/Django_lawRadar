ca1752239474ba15e93218182f793ec26732f748
sori <alsldjsthflone@daum.net>
2025-05-20 18:10:33 +0900

    모델 등록

--- geovote/admin.py
+++ geovote/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Member, District, Party, Committee, Bill, Vote
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(District)
+admin.site.register(Party)
+admin.site.register(Committee)
+admin.site.register(Bill)
+admin.site.register(Vote)
