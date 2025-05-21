b13190c1326b0c57240597d4f9eb18dee4ac5e2c
seoyoung <1203sylim@gmail.com>
2025-05-21 17:05:14 +0900

    bill_vote migration

--- billview/models.py
+++ billview/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 
 # bill
+from django.db import models
+
 class Bill(models.Model):
-    bill_id = models.CharField(max_length=50, unique=True)  # 법안 고유 ID
-    description = models.TextField(blank=True, null=True)  # 법안 설명 전체 텍스트
+    age = models.IntegerField()
+    title = models.CharField(max_length=255)                 
+    bill_id = models.CharField(max_length=100, unique=True)  
+    bill_number = models.CharField(max_length=100, unique=True)
+    content = models.TextField(blank=True, null=True)    
     
     def __str__(self):
-        return self.bill_id
+        return self.title
