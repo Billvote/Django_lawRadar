258021ef9a56f821c8c557fe458716f96662b622
sori <alsldjsthflone@daum.net>
2025-06-12 11:53:32 +0900

    search 해시태그

--- geovote/import_db.py
+++ geovote/import_db.py
@@ -298,7 +298,7 @@ import_ages(csv_path / f'age.csv')
 import_parties(csv_path / f'party.csv')
 import_districts(csv_path / f'district.csv')
 import_members(csv_path / f'member.csv')
-import_bills(csv_path / f'bill_labeled(1).csv')
+import_bills(csv_path / f'bill(2).csv')
 import_votes(csv_path / f'vote.csv')
 
 print(f"데이터 임포트 완료")
