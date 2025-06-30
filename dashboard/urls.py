21ac3ae248f6e328edfd3722e95be7ecf28b48d2
sori <alsldjsthflone@daum.net>
2025-06-30 11:22:55 +0900

    nav 수정

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,6 @@ app_name = "dashboard"   # 네임스페이스 ─ {% url 'dashboard:dashboard' 2
 
 urlpatterns = [
     path('<int:congress_num>/', dashboard_v.dashboard,        name='dashboard'),
+    path('second/<int:congress_num>/', dashboard_v.second_board,        name='second_board'),
     path('api/cluster_chart/',  dashboard_v.cluster_chart_api, name='cluster_chart_api'),
 ]
